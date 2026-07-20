ed6ab7292196b9829f77b74478aaa4b8f333a161
tanay ranka collage <tanayranka42@gmail.com>
2026-07-20 12:55:44 +0530

    Update prac2.c

--- Practical 2/prac2.c
+++ Practical 2/prac2.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int no,pos;
-    printf("Enter the number: ");
-    scanf("%d", &no);
-    
-    printf("Enter a nth position to check: ");
-    scanf("%d", &pos);
-    int t = pos - 1;
-    if((no & 1<<t) > 0){
-        printf("\nyes the nth bit is set to 1");
-    }else{
-        printf("\nno the nth bit is set to 0");
-    }
-    
-    
-    return 0;
-}
