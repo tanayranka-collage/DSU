6d64c538e132111ecb55e2519ea8c863f177661f
tanay ranka collage <tanayranka42@gmail.com>
2026-07-27 12:48:53 +0530

    Create excersice 3.cpp

---
+++ Practical 2/practical 2C/excersice 3.cpp
@@ -0,0 +1,23 @@
+//code b y tanayranka sycse b 8
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter length: ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout << "Enter value: ";
+        cin >> arr[i];
+    }
+    int counter = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i] % 2 == 0){
+            counter++;
+        }
+    }
+    
+    cout << endl;
+    cout << "EVEN numbers in array are: " << counter << endl;
+    return 0;
+}
