643c0bbe14b14ec37421e21091118244dacff742
tanay ranka collage <tanayranka42@gmail.com>
2026-07-27 12:45:26 +0530

    Create excersice 1.cpp

---
+++ Practical 2/practical 2C/excersice 1.cpp
@@ -0,0 +1,14 @@
+// code by tanay ranka sycse b 8
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cout << "Enter a number: ";
+    cin >> t;
+    if(t % 2 == 0){
+        cout << "Number is divisible by 2";
+    }else{
+        cout << "Number is not divisible by 2.";
+    }
+    return 0;
+}
