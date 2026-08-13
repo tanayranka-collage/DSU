9fcd6e31f64e6b564751c21b59fbb17840d6c052
tanay ranka collage <tanayranka42@gmail.com>
2026-08-13 11:04:56 +0530

    Create excersice1.c

---
+++ Practical 5/Practical 5A/excersice1.c
@@ -0,0 +1,32 @@
+//code vy tanay ranks sycse b 8
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    printf("Enter length: ");
+    scanf("%d", &n);
+    int *ps = (int*)malloc(n * sizeof(int));
+    for(int i=0;i<n;i++){
+        printf("Enter Value: ");
+        scanf("%d", &ps[i]);
+    }
+    //bubble sort
+    for (int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(ps[j] < ps[j + 1]){
+                int temp = ps[j];
+                ps[j] = ps[j+1];
+                ps[j+1] = temp;
+            }
+        }
+    }
+    printf("\nSorted array");
+    for(int p=0;p<n;p++){
+        printf(" %d ", ps[p]);
+    }
+
+
+    free(ps);
+    return 0;
+}
