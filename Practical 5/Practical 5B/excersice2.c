d44845c59ec13c551f26d4eb8532d86f1e8563f9
tanay ranka collage <tanayranka42@gmail.com>
2026-08-17 11:59:10 +0530

    Create excersice2.c

---
+++ Practical 5/Practical 5B/excersice2.c
@@ -0,0 +1,37 @@
+//code by tanay ranka sycse b 8
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    char temp[100];
+    int n;
+    printf("Enter lenght: ");
+    scanf("%d", &n);
+    getchar();
+    char arr[n][100];
+    for(int i=0;i<n;i++){
+        printf("Enetr Value: ");
+        fgets(arr[i], sizeof(arr[i]), stdin);
+    }
+    //bubble sort:
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            int k=0;
+            while(arr[j][k] != '\0' && arr[j+1][k] != '\0' && arr[j][k] == arr[j+1][k]){
+                k++;
+            }
+            if(arr[j][k] > arr[j+1][k]){
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[j+1]);
+                strcpy(arr[j+1], temp);
+            }
+        }
+    }
+    printf("\nSorted Array:\n");
+    for(int i=0;i<n;i++){
+        printf(" %s ", arr[i]);
+    }
+
+    return 0;
+}
