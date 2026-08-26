f4c38e71baac237934092d4c1faf92151e75aebe
tanay ranka collage <tanayranka42@gmail.com>
2026-08-26 22:00:16 +0530

    Create prac6a.c

---
+++ Practical 6/Practical 6A/prac6a.c
@@ -0,0 +1,33 @@
+//code by tanay ranka sycse b 8
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter length: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Values: ");
+        scanf("%d", &arr[i]);
+    }
+    //selection sort
+    for(int i=0;i<n-1;i++){
+        int min_index = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[min_index]){
+                min_index = j;
+            }
+
+        }
+        int temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+    }
+    printf("\nSorted Array: ");
+    for(int i=0;i<n;i++){
+        printf(" %d ", arr[i]);
+    }
+
+    return 0;
+}
