d829be68d7247060a92ebea8728df976cd81ac84
tanay ranka collage <tanayranka42@gmail.com>
2026-08-27 20:01:41 +0530

    Create excersice2.c

---
+++ Practical 6/Practical 6A/excersice2.c
@@ -0,0 +1,36 @@
+//code by tanay ranka sycse b 8
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter length: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Values: ");
+        scanf("%d", &arr[i]);
+    }
+    //selection sort
+    int count = 0;
+    for(int i=0;i<n-1;i++){
+        int min_index = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[min_index]){
+                min_index = j;
+                count++;
+            }
+
+        }
+        int temp = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = temp;
+    }
+    printf("\nSorted Array: ");
+    for(int i=0;i<n;i++){
+        printf(" %d ", arr[i]);
+    }
+    printf("\nComparisions: %d", count);
+
+    return 0;
+}
